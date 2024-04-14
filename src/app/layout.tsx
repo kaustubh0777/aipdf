9004873988bc26582aa9ee7a5ac9068a89737dab
Kaustubh Pathak <kaustuap555@gmail.com>
2024-04-14 23:21:13 +0530

    updated navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import Navbar from "@/components/Navbar";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +21,11 @@ export default function RootLayout({
     <html lang="en" className="ligth">
       <body className={cn(
         'min-h-screen font-sans antialiased grainy',inter.className
-      )}>{children}</body>
+      )}>
+        <Navbar/>
+
+        {children}
+        </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -42,7 +42,7 @@ export default function Home() {
             <div
               style={{
                 clipPath:
-                  'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+                  "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
               }}
               className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
             />
@@ -63,7 +63,92 @@ export default function Home() {
               </div>
             </div>
           </div>
+
+          <div
+            aria-hidden="true"
+            className="pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+          >
+            <div
+              style={{
+                clipPath:
+                  "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+              }}
+              className="relative left-[calc(50%-13rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-36rem)] sm:w-[72.1875rem]"
+            />
+          </div>
         </div>
+      </div>
+      <div className="mx-auto mb-32 mt-32 max-w-5xl sm:mt-56">
+        <div className="mb-12 px-6 lg:px-8">
+          <div className="mx-auto max-w-2xl sm:text-center">
+            <h2 className="mt-2 font-bold text-4xl text-gray-900 sm:text-5xl">
+              Start Chatting in minutes
+            </h2>
+            <p className="mt-4 text-lg text-gray-600">
+              Chatting to your PDF Files has never been easier before AIPDF
+            </p>
+          </div>
+        </div>
+
+        <ol className=" my-8 space-y-4 pt-8 md:flex md:space-x-12 md:space-y-0">
+          <li className=" md:flex-1">
+            <div className=" flex flex-col space-y-2 border-1-4 border-zinc-300 py-2 pl-4 md:border-1-0 md:border-t-2 md:pb-0 md:pl-0 md-pt-4">
+              <span className="text-sm font-medium text-blue-600">Step 1</span>
+              <span className="text-xl font-semibold ">
+                Sign up for an account
+              </span>
+              <span className="mt-2 text-zinc-700">
+                Either starting out with an free plan or choose our{" "}
+                <Link
+                  href="/pricing"
+                  className="text-blue-700 underline underline-offset-2"
+                >
+                  pro plan
+                </Link>
+              </span>
+            </div>
+          </li>
+          <li className=" md:flex-1">
+            <div className=" flex flex-col space-y-2 border-1-4 border-zinc-300 py-2 pl-4 md:border-1-0 md:border-t-2 md:pb-0 md:pl-0 md-pt-4">
+              <span className="text-sm font-medium text-blue-600">Step 2</span>
+              <span className="text-xl font-semibold ">
+               Upload your PDF file
+              </span>
+              <span className="mt-2 text-zinc-700">
+                We&apos;ll process your file and make it ready to chat with
+                
+              </span>
+            </div>
+          </li>
+          <li className=" md:flex-1">
+            <div className=" flex flex-col space-y-2 border-1-4 border-zinc-300 py-2 pl-4 md:border-1-0 md:border-t-2 md:pb-0 md:pl-0 md-pt-4">
+              <span className="text-sm font-medium text-blue-600">Step 3</span>
+              <span className="text-xl font-semibold ">
+              Start Asking Questions
+              </span>
+              <span className="mt-2 text-zinc-700">
+                It&apos;s that simple . Try out AIPDF Today. It really takes less then a minute
+                
+              </span>
+            </div>
+          </li>
+        </ol>
+
+        <div className="mx-auto max-w-5xl px-6 lg:px-8">
+              <div className="mt-16 flow-root sm:mt-24">
+                <div className=" -m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
+                  <Image
+                    src="/file-upload-preview.jpg"
+                    alt="upload preview"
+                    width={800}
+                    height={600}
+                    quality={100}
+                    className="rounded-md bg-white p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-gray-900/10"
+                  />
+                </div>
+              </div>
+            </div>
+        
       </div>
     </>
   );
